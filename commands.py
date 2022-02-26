2953cee51ccaff9770337b8653ced1c41c37e1ce
Blaz Korecic <blaz.korecic@ug.uchile.cl>
2022-02-26 01:47:45 -0300

    Add slashear command

--- commands.py
+++ commands.py
@@ -1,5 +1,9 @@
+import argparse
+from ast import arguments
+from distutils import text_file
 import os
 from datetime import datetime
+from typing import Text
 from utils import generate_acronym, get_arg, try_msg
 
 import data
@@ -57,6 +61,19 @@ def siglar(update, context):
             chat_id=update.message.chat_id,
             parse_mode="HTML",
             text=message)
+
+
+def slashear(update, context):
+    log_command(update)
+    if update.message.reply_to_message:
+        words = update.message.reply_to_message.text.split()
+        response = "/" + words[0].lower()
+        for word in words[1:]:
+            response += word.capitalize()
+        try_msg(context.bot,
+                chat_id=update.message.chat_id,
+                parse_mode="HTML",
+                text=response)
 
 
 # Admin Commands
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import data
 from telegram.ext import CommandHandler, Filters
 
 from bot import updater, dp
-from commands import start, tup, desiglar, siglar, get_log
+from commands import start, tup, desiglar, siglar,  slashear, get_log
 from config.auth import admin_ids
 
 
@@ -13,6 +13,7 @@ def main():
     dp.add_handler(CommandHandler('tup', tup))
     dp.add_handler(CommandHandler('desiglar', desiglar))
     dp.add_handler(CommandHandler('siglar', siglar))
+    dp.add_handler(CommandHandler('slashear', slashear))
     # Admin commands
     dp.add_handler(CommandHandler('get_log', get_log,
                    filters=Filters.user(admin_ids)))
