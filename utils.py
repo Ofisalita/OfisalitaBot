4b17cd168733aebc0d6195509d4d5fa7e43e8cbb
Diego Wistuba <wistuba@niclabs.cl>
2023-03-11 19:09:58 -0300

    Added function to send a sticker

--- utils.py
+++ utils.py
@@ -1,9 +1,11 @@
 import random
 import re
-from config.logger import logger
 from string import ascii_lowercase
+
 from telegram import Update, Bot, TelegramError, constants as tg_constants
 from telegram.ext import CallbackContext
+
+from config.logger import logger
 
 word_file = "static/words.txt"
 WORDS = open(word_file).read().splitlines()
@@ -66,6 +68,33 @@ def try_edit(bot: Bot, attempts: int = 2, **params) -> None:
         logger.error((
             f"Max attempts reached for chat {str(chat_id)}."
             "Aborting edit and raising exception."
+        )
+        )
+
+
+def try_sticker(bot: Bot, attempts: int = 2, **params) -> None:
+    """
+    Make multiple attempts to send a sticker.
+    """
+    chat_id = params["chat_id"]
+    attempt = 1
+    while attempt <= attempts:
+        try:
+            bot.send_sticker(**params)
+        except TelegramError as e:
+            logger.error((
+                f"[Attempt {attempt}/{attempts}] Stickering chat {chat_id} "
+                f"raised following error: {type(e).__name__}: {e}"
+            )
+            )
+        else:
+            break
+        attempt += 1
+
+    if attempt > attempts:
+        logger.error((
+            f"Max attempts reached for chat {str(chat_id)}."
+            "Aborting message and raising exception."
         )
         )
 
