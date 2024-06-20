8771ad1008c8911f7f9e153cd2febb996e4e3176
Sebastián Cisneros <sebastian.cisneros384@gmail.com>
2024-06-20 04:30:03 -0400

    remove: Debug func

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from commands.admin import get_log, prohibir
 from commands.counter import contador, sumar, restar
 from commands.list import lista, agregar, quitar, editar, deslistar
 from commands.response import start, tup, gracias, weekly_poll, reply_hello
-from commands.summary import resumir, get_last_n, button
+from commands.summary import resumir, button
 from commands.text import slashear, uwuspeech, repetir
 from commands.gpt import reply_gpt, reply_fill, desigliar
 
@@ -92,7 +92,6 @@ def main():
 
     # Summary
     add_command('resumir', resumir)
-    add_command('get_last_n', get_last_n)
 
     dp.add_handler(CallbackQueryHandler(button))
 
