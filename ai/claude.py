5134b285a7f45500eb4660dd04b6730e1eec942c
Sebastián Cisneros <sebastian.cisneros384@gmail.com>
2024-07-15 20:45:00 -0400

    fix: Claude's max_tokens

--- ai/claude.py
+++ ai/claude.py
@@ -19,7 +19,9 @@ class ClaudeGPTClient(AbstractGenAIClient):
     def generate(
         self, conversation: list[GenAIMessage], system: str = None, **kwargs
     ) -> GenAIResponse:
-        max_tokens = kwargs.pop("max_tokens", 1000)
+        max_tokens = kwargs.pop(
+            "max_tokens", 8192 if self.model.startswith("claude-3-5-sonnet") else 4096
+        )
         request = self.client.messages.create(
             model=self.model,
             max_tokens=max_tokens,
